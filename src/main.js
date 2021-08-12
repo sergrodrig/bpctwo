1f3fd3573623122de0499c07a4f1b85a61f9e746
Sergio Rodriguez <sergrodrig@gmail.com>
2021-08-12 15:21:51 -0400

    realizada una copia de la db de firestore hacia el repositorio local para poder restaurarla cuando se necesite en el emulador

--- src/main.js
+++ src/main.js
@@ -16,11 +16,11 @@ firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 const auth = firebase.auth();
 const functions = firebase.functions();
-// if (location.hostname === "localhost") {
-//   db.useEmulator("localhost", 8888);
-//   auth.useEmulator("http://localhost:9099");
-//   functions.useEmulator("localhost", 5001);
-// }
+if (location.hostname === "localhost") {
+  db.useEmulator("localhost", 8888);
+  auth.useEmulator("http://localhost:9099");
+  functions.useEmulator("localhost", 5001);
+}
 
 // vue app instance
 const bpctwo = createApp(App);
